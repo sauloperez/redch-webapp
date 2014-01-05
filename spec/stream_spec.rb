8bb9872e08b81e8115686d5686ac47ef8bcd5707
Pau Pérez <saulopefa@gmail.com>
2014-01-05 11:22:31 +0100

    Test Event Stream Format compliance

--- spec/stream_spec.rb
+++ spec/stream_spec.rb
@@ -7,7 +7,7 @@ describe "/stream" do
 
   before { mock_amqp }
 
-  it 'should open a streaming connection' do
+  it 'should open a event-stream streaming connection' do
     @event_machine.run do
       get '/stream', provides: 'text/event-stream'
       expect(last_response.content_type).to eq 'text/event-stream;charset=utf-8'
@@ -40,6 +40,14 @@ describe "/stream" do
 
     it 'forwards each message to the clients' do
       em.run do
+        subscription.to 'samples'
+        subscription.exchange.publish "some observation", routing_key: ''
+        expect(@stream).to receive(:<<).with(/some observation/)
+      end
+    end
+
+    it 'messages conform to the Event Stream Format' do
+      @event_machine.run do
         subscription.to 'samples'
         subscription.exchange.publish "Hello, world!", routing_key: ''
         expect(stream).to receive(:<<).with("data: Hello, world!\n\n")
