5b5546242b44dbbcf57a91e662230724f19b2644
Pau Pérez <saulopefa@gmail.com>
2014-02-22 22:51:16 +0100

    Update staging configuration for vagrant VM plus foreman export to upstart (the latter not working yet)

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,18 +2,17 @@
 lock '3.1.0'
 
 set :application, 'redch'
-set :repo_url, 'git@github.com:sauloperez/redch-webapp.git'
-
-ssh_options[:port] = 2222
+set :repo_url, 'git://github.com/sauloperez/redch-webapp.git'
 
 set :user, "vagrant"
 set :group, "vagrant"
 
 # Default branch is :master
+set :branch, 'develop'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/var/www/redch'
+set :deploy_to, '/var/redch'
 
 set :use_sudo, false
 
@@ -44,16 +43,53 @@ set :copy_strategy, :export
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
+# Required when not using rvm or rbenv
+SSHKit.config.command_map[:rake]  = "bundle exec rake"
 
+namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
+      sudo '/etc/init.d/nginx restart'
     end
   end
 
   after :publishing, :restart
+end
+
+namespace :foreman do
+  desc "Export the Procfile to Ubuntu's upstart scripts"
+  task :export do
+    on roles(:app), in: :sequence, wait: 5 do
+      run "cd #{current_path}"
+      # TODO try to get application and user values from variables if possible
+      sudo "bundle exec foreman export upstart /etc/init -a redch -u vagrant"
+    end
+  end
+
+  desc "Start the application services"
+  task :start do
+    on roles :app do
+      sudo "start #{application}"
+    end
+  end
+
+  desc "Stop the application services"
+  task :stop do
+    on roles :app do
+      sudo "stop #{application}"
+    end
+  end
+
+  desc "Restart the application services"
+  task :restart do
+    on roles :app do
+       run "sudo start #{application} || sudo restart #{application}"
+    end
+  end
 
 end
+
+after 'deploy:finished', 'foreman:export'
+after 'deploy:finished', 'foreman:restart'
+
