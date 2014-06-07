aeba1a35ed62eba3dc466b377ef088da292e2b9b
Pau Pérez <saulopefa@gmail.com>
2014-06-07 22:15:39 +0200

    Symlink app's log to shared through capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,8 +20,7 @@ set :copy_strategy, :export
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
 
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
