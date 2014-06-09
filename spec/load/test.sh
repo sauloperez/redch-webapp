a12e635ab7be142563be4c1e34015fe65a75d90b
Pau Pérez <saulopefa@gmail.com>
2014-06-09 19:03:12 +0200

    Print output destination

--- spec/load/test.sh
+++ spec/load/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HOST="http://54.72.161.42/"
+HOST="http://54.72.161.42"
 
 N=$1
 C=$2
@@ -13,6 +13,8 @@ OUTPUT_PATH="output/$FILENAME.txt"
 
 # Execute Apache Bench test
 /usr/sbin/ab -k -n $N -c $C -g $DATA_PATH $URL > $OUTPUT_PATH
+
+echo "Output stored in '$OUTPUT_PATH'"
 
 # Plot the resulting data
 ./plot.sh $FILENAME $N $C
