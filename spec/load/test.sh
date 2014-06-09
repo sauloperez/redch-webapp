2ec2f71f82c5fe7a58e47f4210b716cfa2c6deca
Pau Pérez <saulopefa@gmail.com>
2014-06-09 10:12:04 +0200

    Automate ab test and plot with test.sh

---
+++ spec/load/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+HOST="http://54.72.161.42/"
+
+N=$1
+C=$2
+PATH=$3
+
+URL=$HOST/$PATH
+FILENAME="kn${N}c$C"
+DATA_PATH="data/$FILENAME.txt"
+OUTPUT_PATH="output/$FILENAME.txt"
+
+# Execute Apache Bench test
+/usr/sbin/ab -k -n $N -c $C -g $DATA_PATH $URL > $OUTPUT_PATH
+
+# Plot the resulting data
+./plot.sh $FILENAME $N $C
+
